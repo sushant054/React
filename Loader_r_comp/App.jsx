16612120f6367af4748fe55044e6cdd55f0db41b
sushant054 <sushantnalage54@gmail.com>
2025-02-10 18:25:43 +0530

    Loader react component (for loading...)

--- Loader_r_comp/App.jsx
+++ Loader_r_comp/App.jsx
@@ -9,3 +9,6 @@ import Loader from './processing';
 }
 
 export default App
+
+//for material ui progress (loding..)
+// https://mui.com/material-ui/react-progress/?srsltid=AfmBOoodxejd5Q1P3Xjc-EXJ9xMMGVIsYrUiEcb1x4NLGVB8dNqIWx6X
