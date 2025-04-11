df93e7b25dff4b6ba4e28c69c8c1a8fa21ac7ced
sushant054 <sushantnalage54@gmail.com>
2025-04-11 13:20:17 +0530

    Investment_calculator..

---
+++ Investment_calculator/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../assets/investment-Calculator-logo.png';
+export default function Header() {
+  return (
+    <div id="header"><img src={logo} alt="Logo showing a money bag"/>
+    <h1>Investment Calculator</h1>
+    </div>
+  );
+}
